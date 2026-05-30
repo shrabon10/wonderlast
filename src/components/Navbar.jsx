8582d0d33e71ef64892ae6ee370e8422ced26b41
shrabon10 <omarfaruksrabon@gmail.com>
2026-05-30 19:40:46 +0600

    Environment SetUp & Navbar Footer Banner Section Has been Created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,47 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className='flex justify-between p-5'>
+            <ul className='flex gap-4'>
+                <li>
+                    <Link href="/">Home</Link>
+                </li>
+                <li>
+                    <Link href="/destinations">Destinations</Link>
+                </li>
+                <li>
+                    <Link href="/my-bookings">My Bookings</Link>
+                </li>
+                <li>
+                    <Link href="/admin">Admin</Link>
+                </li>
+            </ul>
+
+            <div>
+                <Image
+                    src="/assets/Wanderlast.png"
+                    alt="Logo"
+                    width={150}
+                    height={150}
+                />
+            </div>
+            <ul className='flex gap-4'  >
+                <li>
+                    <Link href="/profile">Profile</Link>
+                </li>
+                <li>
+                    <Link href="/login">LogIn</Link>
+                </li>
+                <li>
+                    <Link href="/signup">Sign Up</Link>
+                </li>
+
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
